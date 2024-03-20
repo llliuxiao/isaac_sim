b2466cc9642ad1d9fc05fc5964dfa302e62ff71f
LIU Xiao <xiao.liu@geometryrobot.com>
2024-03-20 10:03:47 +0800

    fix error yaml file path, remove roscore from shell

--- shell/multi_robot_navigation.sh
+++ shell/multi_robot_navigation.sh
@@ -13,14 +13,8 @@ session="multi_robot_navigation"
 tmux new-session -d -s $session
 
 window=0
-tmux rename-window -t $session:$window "roscore"
-tmux send-keys -t $session:$window "roscore" C-m
-echo "start roscore"
-sleep 2s
-
-window=$((window + 1))
 tmux new-window -t $session:"$window" -n "rviz"
-tmux send-keys -t $session:"$window" "rosparam load src/isaac_sim.config/multi_robots_config" C-m
+tmux send-keys -t $session:"$window" "rosparam load ~/isaac_sim_ws/src/isaac_sim/config/multi_robots_config.yaml" C-m
 tmux send-keys -t $session:"$window" "rviz -d ~/isaac_sim_ws/src/isaac_sim/rviz/carter.rviz" C-m
 echo "start rviz and load multi robot parameters"
 
@@ -30,7 +24,7 @@ do
   window=$((window + 1))
   tmux new-window -t $session:"$window" -n "Carter_$i"
   tmux send-keys -t $session:"$window" "source ~/isaac_sim_ws/devel/setup.bash" C-m
-  tmux send-keys -t $session:"$window" "roslaunch isaac_sim multi_robot_navigation.launch prefix:=Carter_${i} launch_rviz:=false disable_teb:=false" C-m
+  tmux send-keys -t $session:"$window" "roslaunch isaac_sim multi_robot_navigation.launch prefix:=Carter_${i} launch_rviz:=false disable_teb:=true" C-m
   echo "No.$i robot navigation finish launching"
   i=$((i + 1))
 done
